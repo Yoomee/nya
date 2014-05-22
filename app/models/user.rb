af4ccb21d32f9094d6bdcc2879709f895f28fe24
David Thompson <dtt101@gmail.com>
2014-05-22 15:50:29 +0100

    Add user and project relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
   include YmUsers::User
 
+  has_many :projects
+
   validates_presence_of :first_name, :last_name
   validates :email, :email => true, :presence => true, :uniqueness => true
   validates_presence_of :password, :on => :create
