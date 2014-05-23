91388e669cb97c163009bafc4de9d9b0d135ecc0
David Thompson <dtt101@gmail.com>
2014-05-23 10:58:04 +0100

    Add project model to ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,15 +1,15 @@
 # Don't delete comments! They are used in gems for adding abilities
 class Ability
-  
+
   include CanCan::Ability
-  
+
   def initialize(user)
-    
+
     # open ability
     can :show, ContentPage
-    
+
     if user.try(:admin?)
-      can :manage, :all      
+      can :manage, :all
       # admin ability
     elsif user
       # user ability
@@ -17,9 +17,11 @@ class Ability
       can [:read, :create, :file], Post
       can [:update, :destroy], Post, :user_id => user.id
       can [:create], Comment
-      can :manage, User, :id => user.id      
+      can :manage, User, :id => user.id
+      can [:read, :create], Project
+      can [:update, :destroy], Project, :user_id => user.id
     end
-    
+
   end
-  
+
 end
