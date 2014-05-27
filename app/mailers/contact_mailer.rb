349936df96b53e7ce8ce809fc16f6ece18c2e854
Edward Andrews-Hodgson <edward@yoomee.com>
2014-05-27 13:59:16 +0100

    Send the email to the right person

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,10 @@
 class ContactMailer < ActionMailer::Base
   default from: Settings.site_email
+  default to: Settings.site_email
   default bcc: 'support@yoomee.com'
 
   def contact_enquiry(contact_enquiry)
     @contact_enquiry = contact_enquiry
-    mail(:to => @contact_enquiry.email, :subject => "#{Settings.site_name} - a new message from the contact page")
+    mail(:subject => "#{Settings.site_name} - a new message from the contact page")
   end
 end
