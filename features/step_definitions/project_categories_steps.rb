2ba6891bbea9ec70e2a1fbb6804ad3f0d7688ed1
David Thompson <dtt101@gmail.com>
2014-05-27 10:18:49 +0100

    Feature and step definition for project categories

---
+++ features/step_definitions/project_categories_steps.rb
@@ -0,0 +1,11 @@
+Given(/^a list of project categories$/) do
+  @project_categories = create_list(:project_categories, x.to_i)
+end
+
+When(/^I select a project category$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can see projects for the selected category$/) do
+  pending # express the regexp above with the code you wish you had
+end
---
+++ spec/factories/project_categories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :project_category do
+    title "Art"
+    description "Example category description"
+  end
+end
