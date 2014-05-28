5044984ea1f8d8c4ec3c2e928ea50592d9622d70
David Thompson <dtt101@gmail.com>
2014-05-28 16:17:42 +0100

    Use a sequence instead of a number for user factory email

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     first_name 'John'
     last_name 'Doe'
-    email { "user_#{rand(1000).to_s}@yoomee.com" }
+    sequence(:email) { |n| "user_#{n}@yoomee.com" }
     password 'password'
     role ''
   end
