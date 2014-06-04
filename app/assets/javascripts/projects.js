804bf0dd1a81ec18d9e589808b320b58f0b32185
David Thompson <dtt101@gmail.com>
2014-06-04 11:57:23 +0100

    Add first pass at create project wizard

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,14 +1,14 @@
 // supports project create form
 $(document).ready(function() {
+
   $('#project-create').parsley();
 
   $('#project-create .next-btn').on("click", function(e) {
     e.preventDefault();
-    console.log('pressed');
     var current = $(this).data('currentGroup');
     var next = $(this).data('nextGroup');
 
-    // validate going forward through the form
+    // validate ony going forward through the form
     if (next > current)
       if (false === $('#project-create').parsley().validate('group' + current))
         return;
@@ -17,12 +17,10 @@ $(document).ready(function() {
     $('.group' + current)
       .removeClass('show')
       .addClass('hidden');
-
     $('.group' + next)
       .removeClass('hidden')
       .addClass('show');
 
-    // TODO: if last step hide self in favour of create button
     // TODO: do we need a back button
   });
 });
