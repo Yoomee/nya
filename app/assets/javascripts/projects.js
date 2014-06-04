a3fb3dfe5b22a91a78354f7aeb27cb1b8ee3d4f7
David Thompson <dtt101@gmail.com>
2014-06-04 12:54:25 +0100

    Add check for form before trying to activate parsley

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,29 +1,31 @@
 // supports project create form
 $(document).ready(function() {
 
-  $('#project-create').parsley();
+  if ($('form#project-create').length) {
+    $('form#project-create').parsley();
 
-  $('#project-create .next-btn').on("click", function(e) {
-    e.preventDefault();
-    var current = $(this).data('currentGroup');
-    var next = $(this).data('nextGroup');
+    $('#project-create .next-btn').on("click", function(e) {
+      e.preventDefault();
+      var current = $(this).data('currentGroup');
+      var next = $(this).data('nextGroup');
 
-    // validate ony going forward through the form
-    if (next > current)
-      if (false === $('#project-create').parsley().validate('group' + current)) {
-        // make sure final submit button is reset to default state
-        $('#create-submit-btn').removeClass('disabled').val('Create your project');
-        return;
-      }
+      // validate ony going forward through the form
+      if (next > current)
+        if (false === $('#project-create').parsley().validate('group' + current)) {
+          // make sure final submit button is reset to default state
+          $('#create-submit-btn').removeClass('disabled').val('Create your project');
+          return;
+        }
 
-    // move to next step when valid
-    $('.group' + current)
-      .removeClass('show')
-      .addClass('hidden');
-    $('.group' + next)
-      .removeClass('hidden')
-      .addClass('show');
+      // move to next step when valid
+      $('.group' + current)
+        .removeClass('show')
+        .addClass('hidden');
+      $('.group' + next)
+        .removeClass('hidden')
+        .addClass('show');
 
-    // TODO: do we need a back button
-  });
+      // TODO: do we need a back button
+    });
+  }
 });
