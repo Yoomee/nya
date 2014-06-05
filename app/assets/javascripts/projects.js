14dff87bbc93d367db7e4c137a5e406fc09b1605
David Thompson <dtt101@gmail.com>
2014-06-05 16:09:12 +0100

    Fix bug in interaction with UJS and parsley on project create form

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -4,6 +4,12 @@ $(document).ready(function() {
   if ($('form#project-create').length) {
     $('form#project-create').parsley();
 
+    // work around for parsley / ujs issue: https://github.com/guillaumepotier/Parsley.js/issues/561
+    $('#project-create').parsley().subscribe('parsley:field:error', function (formInstance) {
+      // make sure final submit button is reset to default state
+      $('#create-submit-btn').removeClass('disabled').val('Create your project');
+    });
+
     $('#project-create .next-btn').on("click", function(e) {
       e.preventDefault();
       var current = $(this).data('currentGroup');
