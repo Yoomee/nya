753ce76b5a05672ad795480f8aa1d58cd2441f90
Edward Andrews-Hodgson <edward@yoomee.com>
2014-09-23 12:42:17 +0100

    So you do need this - oops

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
 
     var total_steps = 4;
 
+     // work around for parsley / ujs issue: https://github.com/guillaumepotier/Parsley.js/issues/561
+    $('#project-create').parsley().subscribe('parsley:field:error', function (formInstance) {
+      // make sure final submit button is reset to default state
+      $('#create-submit-btn').removeClass('disabled').val('Create your project');
+    });
+
     $('#project-create .next-btn').on("click", function(e) {
       e.preventDefault();
       var current = $(this).data('currentGroup');
